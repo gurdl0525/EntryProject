8e038a0a03b54f06ad470bcef24302d55c261e6d
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 12:58:49 +0900

    📌POST:::UserDetailsService 구현

--- src/main/java/com/example/assignment/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/example/assignment/global/security/auth/AuthDetailsService.java
@@ -1,2 +1,20 @@
-package com.example.assignment.global.security.auth;public class AuthDetailsService {
+package com.example.assignment.global.security.auth;
+
+import com.example.assignment.domain.entity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(username)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("NOT FOUND USERNAME"));
+    }
 }
