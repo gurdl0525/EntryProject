321a0242f2e940a2955be45410910c34dbfdd166
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:13:35 +0900

    📌POST:::WebMvcConfigurer 구현

--- src/main/java/com/example/assignment/global/config/WebMvcConfig.java
+++ src/main/java/com/example/assignment/global/config/WebMvcConfig.java
@@ -1,2 +1,17 @@
-package com.example.assignment.global.config;public class WebMvcConfig {
+package com.example.assignment.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
 }
