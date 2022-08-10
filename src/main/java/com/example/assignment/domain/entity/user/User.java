7d5dcb9c1bb3568454d76f5fe1d4255724034a01
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:15:13 +0900

    📌POST:::User Entity 생성

--- src/main/java/com/example/assignment/domain/entity/user/User.java
+++ src/main/java/com/example/assignment/domain/entity/user/User.java
@@ -1,2 +1,36 @@
-package com.example.assignment.domain.entity.user;public class User {
+package com.example.assignment.domain.entity.user;
+
+import com.example.assignment.domain.entity.novel.Novel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Builder
+@Getter
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "nickname")
+    private String name;
+
+    @JoinColumn(name = "account_id")
+    private String accountId;
+
+    @JoinColumn(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "novel", cascade = CascadeType.REMOVE)
+    private List<Novel> novelList;
+
 }
