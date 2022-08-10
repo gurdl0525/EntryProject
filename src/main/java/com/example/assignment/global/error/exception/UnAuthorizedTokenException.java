2e87b54110c2de075df156ad586d9f4ac9824dd4
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:12:50 +0900

    📌POST:::UnAuthorizedTokenException 생성

--- src/main/java/com/example/assignment/global/error/exception/UnAuthorizedTokenException.java
+++ src/main/java/com/example/assignment/global/error/exception/UnAuthorizedTokenException.java
@@ -1,2 +1,17 @@
-package com.example.assignment.global.error.exception;public class UnAuthorizedTokenException {
+package com.example.assignment.global.error.exception;
+
+import com.example.assignment.global.error.ErrorCode;
+
+public class UnAuthorizedTokenException extends BaseException {
+    private UnAuthorizedTokenException(){
+        super(ErrorCode.UN_AUTHORIZED_TOKEN_EXCEPTION);
+    }
+
+    private static class SingletonHelper{
+        private static final UnAuthorizedTokenException INSTANCE = new UnAuthorizedTokenException();
+    }
+
+    public static UnAuthorizedTokenException getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
 }
