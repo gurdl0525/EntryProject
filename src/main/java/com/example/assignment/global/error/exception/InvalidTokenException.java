625732b56b02f5676ddf722c5df9135e22e14c0c
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:13:11 +0900

    📌POST:::유효하지 않은 토큰 생성

--- src/main/java/com/example/assignment/global/error/exception/InvalidTokenException.java
+++ src/main/java/com/example/assignment/global/error/exception/InvalidTokenException.java
@@ -1,2 +1,19 @@
-package com.example.assignment.global.error.exception;public class InvalidTokenException {
+package com.example.assignment.global.error.exception;
+
+import com.example.assignment.global.error.ErrorCode;
+
+public class InvalidTokenException extends BaseException {
+
+    private InvalidTokenException(){
+        super(ErrorCode.INVALID_TOKEN);
+    }
+
+    private static class SingletonHelper{
+        private static final InvalidTokenException INSTANCE = new InvalidTokenException();
+    }
+
+    public static InvalidTokenException getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+
 }
