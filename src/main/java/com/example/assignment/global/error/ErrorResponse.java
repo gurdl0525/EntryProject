50213bc31a41cd1e068757d1f976e276546a2780
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:14:42 +0900

    📌POST:::ErrorResponse 생성

--- src/main/java/com/example/assignment/global/error/ErrorResponse.java
+++ src/main/java/com/example/assignment/global/error/ErrorResponse.java
@@ -1,2 +1,20 @@
-package com.example.assignment.global.error;public class ErrorResponse {
+package com.example.assignment.global.error;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    public String message;
+
+    public String convertObjectToJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(this);
+    }
+
 }
