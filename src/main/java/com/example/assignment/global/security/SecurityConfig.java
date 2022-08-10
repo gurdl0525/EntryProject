38ce6e7e17e8e19966186287ff10b115d422653f
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:11:50 +0900

    📌POST:::SecurityConfig설정

--- src/main/java/com/example/assignment/global/security/SecurityConfig.java
+++ src/main/java/com/example/assignment/global/security/SecurityConfig.java
@@ -1,2 +1,44 @@
-package com.example.assignment.global.security;public class SecurityConfig {
+package com.example.assignment.global.security;
+
+import com.example.assignment.global.error.ExceptionHandlerFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().disable()
+                .formLogin().disable()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isCorsRequest).permitAll()
+                .antMatchers().authenticated()
+                .anyRequest().permitAll()
+                .and().apply(new JwtConfigurer(jwtTokenProvider, exceptionHandlerFilter));
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
