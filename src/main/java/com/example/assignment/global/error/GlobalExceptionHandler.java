64c27530903c82f053daec16645f01de8fb1d405
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:04:55 +0900

    📌POST:::GlobalExceptionHandler 생성

---
+++ src/main/java/com/example/assignment/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.assignment.global.error;
+
+import com.example.assignment.global.error.exception.BaseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = BaseException.class)
+    public ResponseEntity baseException(BaseException e){
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(e.getErrorCode().getMessage());
+    }
+}
