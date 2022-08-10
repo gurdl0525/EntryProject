a26cc36ee3214bd29e3c7aa18f60cbfb71abd2df
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:08:35 +0900

    📌POST:::SecurityConfigurerAdapter 구현

--- src/main/java/com/example/assignment/global/security/JwtConfigurer.java
+++ src/main/java/com/example/assignment/global/security/JwtConfigurer.java
@@ -1,2 +1,26 @@
-package com.example.assignment.global.security;public class JwtConfigurer {
+package com.example.assignment.global.security;
+
+import com.example.assignment.global.error.ExceptionHandlerFilter;
+import com.example.assignment.global.error.exception.InvalidTokenException;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    public void configure(@NotNull HttpSecurity builder) throws InvalidTokenException {
+        var jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
+    }
+
 }
