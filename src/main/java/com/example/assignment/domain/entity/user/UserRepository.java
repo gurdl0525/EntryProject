5d0614d5724d1923d791c0aff7505e63130dab1d
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:15:30 +0900

    📌POST:::UserRepository 생성

--- src/main/java/com/example/assignment/domain/entity/user/UserRepository.java
+++ src/main/java/com/example/assignment/domain/entity/user/UserRepository.java
@@ -1,2 +1,13 @@
-package com.example.assignment.domain.entity.user;public interface UserRepository {
+package com.example.assignment.domain.entity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByAccountId(String accountId);
+
 }
