b6005271d40f60f8bd67a233e481cb53b9234065
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:14:58 +0900

    📌POST:::Novel Entity 생성

--- src/main/java/com/example/assignment/domain/entity/novel/Novel.java
+++ src/main/java/com/example/assignment/domain/entity/novel/Novel.java
@@ -1,2 +1,32 @@
-package com.example.assignment.domain.entity.novel;public class Novel {
+package com.example.assignment.domain.entity.novel;
+
+import com.example.assignment.domain.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Table(name = "novel")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Novel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "title")
+    private String title;
+
+    @JoinColumn(name = "main_text")
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
