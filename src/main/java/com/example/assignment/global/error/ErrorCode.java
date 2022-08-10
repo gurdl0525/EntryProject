261df382334275bba13969be279362b65bda8919
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-10 13:06:19 +0900

    📌POST:::ErrorStatus와 message를 받는 enum클래스 생성

--- src/main/java/com/example/assignment/global/error/ErrorCode.java
+++ src/main/java/com/example/assignment/global/error/ErrorCode.java
@@ -1,2 +1,17 @@
-package com.example.assignment.global.error;public class ErrorCode {
+package com.example.assignment.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    UN_AUTHORIZED_TOKEN_EXCEPTION(401, "Un Authorized Token."),
+    INVALID_TOKEN(401, "Invalid Token."),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error.");
+
+    private final Integer status;
+
+    private final String message;
+
 }
