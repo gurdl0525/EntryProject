cb05e77ec8bc591daba46a6ee9e313281228e86e
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 06:51:25 +0900

    📌POST ::: LikesService 생성

---
+++ src/main/java/com/example/assignment/domain/service/likes/LikesService.java
@@ -0,0 +1,9 @@
+package com.example.assignment.domain.service.likes;
+
+import com.example.assignment.domain.controller.dto.response.MessageResponse;
+
+public interface LikesService {
+    MessageResponse like(Long id);
+
+    MessageResponse unLike(Long id);
+}
