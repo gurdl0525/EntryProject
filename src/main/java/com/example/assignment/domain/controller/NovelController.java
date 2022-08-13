02548d66714f27b0d31915279ecd621983d21076
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 06:53:34 +0900

    📌POST ::: NovelController 생성

---
+++ src/main/java/com/example/assignment/domain/controller/NovelController.java
@@ -0,0 +1,43 @@
+package com.example.assignment.domain.controller;
+
+import com.example.assignment.domain.controller.dto.request.EditNovelRequest;
+import com.example.assignment.domain.controller.dto.request.PostNovelRequest;
+import com.example.assignment.domain.controller.dto.response.MessageResponse;
+import com.example.assignment.domain.controller.dto.response.NovelListTitleResponse;
+import com.example.assignment.domain.controller.dto.response.NovelResponse;
+import com.example.assignment.domain.service.novel.NovelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/novel")
+@RequiredArgsConstructor
+public class NovelController {
+
+    private final NovelService novelService;
+
+    @PostMapping
+    public MessageResponse postNovel(@RequestBody PostNovelRequest request){
+        return novelService.postNovel(request);
+    }
+
+    @GetMapping("/{novel-id}")
+    public NovelResponse getNovel(@PathVariable("novel-id") Long id){
+        return novelService.getNovel(id);
+    }
+
+    @PatchMapping("/edit")
+    public NovelResponse editNovel(@RequestBody EditNovelRequest request){
+        return novelService.editNovel(request);
+    }
+
+    @DeleteMapping("{novel-id}")
+    public MessageResponse deleteNovel(@PathVariable("novel-id") Long id){
+        return novelService.deleteNovel(id);
+    }
+
+    @GetMapping("/likes")
+    public NovelListTitleResponse getLikesNovel(){
+        return novelService.getLikesNovel();
+    }
+}
