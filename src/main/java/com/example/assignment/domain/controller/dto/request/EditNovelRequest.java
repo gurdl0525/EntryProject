77920163c71e36ae075b244eabb3ecd11d2ada04
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 06:47:41 +0900

    📌POST ::: 소설 수정하기 dto 생성

---
+++ src/main/java/com/example/assignment/domain/controller/dto/request/EditNovelRequest.java
@@ -0,0 +1,22 @@
+package com.example.assignment.domain.controller.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditNovelRequest {
+
+    private Long id;
+
+    private String title;
+
+    private Long category;
+
+    private String text;
+
+}
