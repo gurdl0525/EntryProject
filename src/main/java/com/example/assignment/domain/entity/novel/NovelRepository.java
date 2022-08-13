c4a231271100526a47abf2d168342534fd1af800
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 06:59:54 +0900

    🎲UPDATE ::: findNovelsByUser 기능 추가

--- src/main/java/com/example/assignment/domain/entity/novel/NovelRepository.java
+++ src/main/java/com/example/assignment/domain/entity/novel/NovelRepository.java
@@ -1,4 +1,14 @@
 package com.example.assignment.domain.entity.novel;
 
-public interface NovelRepository {
+import com.example.assignment.domain.entity.user.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NovelRepository extends CrudRepository<Novel, Long> {
+
+    List<Novel> findNovelsByUser(User user);
+
 }
