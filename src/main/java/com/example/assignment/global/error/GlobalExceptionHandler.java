7b9709704501023109666ba107e1e3d66d108ba7
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 18:50:50 +0900

    🎲UPDATE ::: validationException 핸들링

--- src/main/java/com/example/assignment/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/assignment/global/error/GlobalExceptionHandler.java
@@ -5,11 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = BaseException.class)
     public ResponseEntity<?> baseException(BaseException e){
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(e.getErrorCode().getMessage());
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<?> validException(){
+        return ResponseEntity.status(400)
+                .body("아이디는 영문 소문자 또는 숫자 4자리 이상 8자리 이하로 입력해야 합니다.");
     }
 }
