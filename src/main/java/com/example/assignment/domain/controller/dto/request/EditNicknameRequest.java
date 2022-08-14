3bb0504d7fddec43414cf62691a03cfb978144ab
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 18:56:18 +0900

    📌POST ::: nickname을 담는 dto 생성

---
+++ src/main/java/com/example/assignment/domain/controller/dto/request/EditNicknameRequest.java
@@ -0,0 +1,16 @@
+package com.example.assignment.domain.controller.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditNicknameRequest {
+
+    private String nickname;
+
+}
