c63218ced7a6c072e38b49c11750b075b75df3ba
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 18:51:30 +0900

    🎲UPDATE ::: 코드 정렬

--- src/main/java/com/example/assignment/domain/service/likes/LikesService.java
+++ src/main/java/com/example/assignment/domain/service/likes/LikesService.java
@@ -3,7 +3,9 @@ package com.example.assignment.domain.service.likes;
 import com.example.assignment.domain.controller.dto.response.MessageResponse;
 
 public interface LikesService {
+
     MessageResponse like(Long id);
 
     MessageResponse unLike(Long id);
+
 }
