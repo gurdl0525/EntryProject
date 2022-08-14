738c3f6e13926537e6b07db05536dcae951c9c56
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-14 18:54:38 +0900

    🎲UPDATE ::: 불필요 코드 삭제

--- src/main/java/com/example/assignment/domain/entity/user/UserRepository.java
+++ src/main/java/com/example/assignment/domain/entity/user/UserRepository.java
@@ -1,10 +1,8 @@
 package com.example.assignment.domain.entity.user;
 
-import com.example.assignment.domain.entity.novel.Novel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
